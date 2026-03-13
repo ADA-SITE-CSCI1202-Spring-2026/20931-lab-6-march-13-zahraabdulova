4742834c87ba80e045bf297290d2a5b2a2364dd7
zahraabdulova <zabdulova16110@ada.edu.az>
2026-03-13 09:23:11 +0400

    Create AccountPart1.java

---
+++ AccountPart1.java
@@ -0,0 +1,26 @@
+// Account Class (Part 1)
+public class Account {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    // Constructor
+    public Account(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    // Getter and Setter methods (optional for this variant)
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
